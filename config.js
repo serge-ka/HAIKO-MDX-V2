ba182da11dd6d2afed001f3435466f008ac48084
PROFESSEURMDX <professeurmdx@gmail.com>
2025-05-20 03:04:01 -0400

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ ANTI_LINK: process.env.ANTI_LINK || "true",
 // make anti link true,false for groups 
 MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
-MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/r30dc6.jpg",
+MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://i.ibb.co/Kp634Kg9/3895.jpg",
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || "±",
 // add your prifix for bot   
@@ -43,7 +43,7 @@ OWNER_NAME: process.env.OWNER_NAME || "ᴘʀᴏғᴇssᴇᴜʀ ᴛᴇᴄʜ",
 // add bot owner name
 DESCRIPTION: process.env.DESCRIPTION || "*© ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʜᴀɪᴋᴏ ᴍᴅx*",
 // add bot owner name    
-ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/r30dc6.jpg",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/JjRxQqvN/9257.jpg",
 // add img for alive msg
 LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *HAIKO-MDX-V2*👻",
 // add alive msg here 
