1485d4667610b2a5f0decb288f760a991b827cff
PROFESSEURMDX <professeurmdx@gmail.com>
2025-05-20 02:31:24 -0400

    Update mention-reply.js

--- plugins/mention-reply.js
+++ plugins/mention-reply.js
@@ -45,7 +45,7 @@ cmd({
             mediaType: 1,
             renderLargerThumbnail: true,
             thumbnail: thumbnailBuffer,
-            mediaUrl: "https://files.catbox.moe/cad2f0.jpg", // Static image URL
+            mediaUrl: "https://i.ibb.co/Kp634Kg9/3895.jpg", // Static image URL
             sourceUrl: "https://wa.me/message/yourself",
             showAdAttribution: true
           }
