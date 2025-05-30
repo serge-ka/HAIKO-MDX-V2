0c76997b7dbc084e00ac3baf8322e2cc750f032b
PROFESSEURMDX <professeurmdx@gmail.com>
2025-05-30 17:52:48 -0400

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -53,7 +53,7 @@ async (conn, mek, m, { from }) => {
 
         // Send audio as per your request
         await conn.sendMessage(from, {
-            audio: { url: 'https://files.catbox.moe/gm2hfz.mp3' }, // Audio URL
+            audio: { url: 'https://files.catbox.moe/gidmqw.mp3' }, // Audio URL
             mimetype: 'audio/mp4',
             ptt: true
         }, { quoted: mek });
