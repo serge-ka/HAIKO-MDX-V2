253bf3a22ff9eaf7fdc806cf8d85ae374dd45b26
PROFESSEURMDX <professeurmdx@gmail.com>
2025-06-08 06:50:33 -0400

    Update config.js

--- config.js
+++ config.js
@@ -25,7 +25,7 @@ MENTION_REPLY: process.env.MENTION_REPLY || "false",
 // make true if want auto voice reply if someone menetion you 
 MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/3nkxx1.jpg",
 // add custom menu and mention reply image url
-PREFIX: process.env.PREFIX || "♡",
+PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
 BOT_NAME: process.env.BOT_NAME || "𝐇𝐀𝐈𝐊𝐎-𝐌𝐃𝐗-𝐕𝟐",
 // add bot namw here for menu
