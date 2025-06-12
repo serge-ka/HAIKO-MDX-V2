56d05930bea1f773ede614a1cfdf07c9e7e8b2ec
PROFESSEURMDX <professeurmdx@gmail.com>
2025-06-12 01:05:31 -0400

    Update tool-pdf.js

--- plugins/tool-pdf.js
+++ plugins/tool-pdf.js
@@ -12,7 +12,7 @@ cmd({
 },
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
-        if (!q) return reply("Please provide the text you want to convert to PDF. *Eg* `.topdf` *megalodon-md*");
+        if (!q) return reply("Please provide the text you want to convert to PDF. *Eg* `.topdf` *haiko-mdx*");
 
         // Create a new PDF document
         const doc = new PDFDocument();
@@ -25,11 +25,11 @@ async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sen
             await conn.sendMessage(from, {
                 document: pdfData,
                 mimetype: 'application/pdf',
-                fileName: 'Megalodon.pdf',
+                fileName: 'Haiko-mdx.pdf',
                 caption: `
 *📄 PDF created successully!*
 
-> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘʀᴏғᴇssᴇᴜʀ ᴛᴇᴄʜ 💜`
+> © ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴅᴇᴠ xᴛʀᴇᴍᴇ💜`
             }, { quoted: mek });
         });
 
