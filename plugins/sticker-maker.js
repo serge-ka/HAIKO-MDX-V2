0ccc55aaabe9cb09c494d13915c16f9d5ea2bd7a
PROFESSEURMDX <professeurmdx@gmail.com>
2025-06-18 16:22:30 -0400

    Update sticker-maker.js

--- plugins/sticker-maker.js
+++ plugins/sticker-maker.js
@@ -21,8 +21,8 @@ cmd(
     },
     async (conn, mek, m, { quoted, args, q, reply, from }) => {
         if (!mek.quoted) return reply(`*REPLY TO ANY STICKER.*`);
-        if (!q) return reply(`*PLEASE PROVIDE A PACK NAME*
-        > EXEMPLE .TAKE PROF-XTREME*`);
+        if (!q) return reply(`*PLEASE PROVIDE A PACK NAME
+        > EXEMPLE .TAKE XTREME*`);
 
         let mime = mek.quoted.mtype;
         let pack = q;
