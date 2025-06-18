3b23bfc6e8d5c3a56a72617e6cb2f83237568e67
PROFESSEURMDX <professeurmdx@gmail.com>
2025-06-17 21:35:42 -0400

    Update group-tagall.js

--- plugins/group-tagall.js
+++ plugins/group-tagall.js
@@ -2,9 +2,11 @@ const config = require('../config')
 const { cmd, commands } = require('../command')
 const { getBuffer, getGroupAdmins, getRandom, h2k, isUrl, Json, runtime, sleep, fetchJson} = require('../lib/functions')
 
+const fallbackPP = 'https://i.ibb.co/KhYC4FY/1221bc0bdd2354b42b293317ff2adbcf-icon.png';
+
 cmd({
     pattern: "tagall",
-    react: "📣",
+    react: "📇",
     alias: ["gc_tagall","appel"],
     desc: "To Tag all Members",
     category: "group",
